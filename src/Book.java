deed401f6a515969c150dc36a39aa05f5976b174
iRUNCHIKK <btkv_irina@icloud.com>
2024-05-01 01:18:15 +0500

    Completed task

---
+++ src/Book.java
@@ -0,0 +1,27 @@
+public class Book {
+    private String name;
+    private Author author;
+    private int publicationYear;
+
+    public Book(String name, int publicationYear, Author author) {
+        this.name = name;
+        this.publicationYear = publicationYear;
+        this.author = author;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //1 объект
+        Author author1 = new Author("Тилли", "Коул");
+        Book book1 = new Book("Тысяча поцелуев, которые невозможно забыть", 2020, author1);
+
+        System.out.println("Год издания: " + book1.getPublicationYear());
+
+        book1.setPublicationYear(2022);
+        System.out.println("Год издания: " + book1.getPublicationYear());
+
+        //2 объект
+        Author author2 = new Author("Стефани", "Майер");
+        Book book2 = new Book("Сумерки", 1989, author2);
+
+        System.out.println("Год издания: " + book2.getPublicationYear());
+
+        book2.setPublicationYear(2005);
+        System.out.println("Год издания: " + book2.getPublicationYear());
     }
 }
